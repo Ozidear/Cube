58a71e00b34bbcbc84e2cff3e6ab1fa1f2a29496
Ozidear <113035159+Ozidear@users.noreply.github.com>
2022-09-07 16:40:42 +0300

    Add files via upload

---
+++ Cube/Cube.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dvarim
+{
+    class Cube
+    {
+        public enum Color { Red, Blue, Green, Yellow }
+
+        Color faceUp, faceDown, faceRight, faceLeft, faceFront, faceBack;
+
+        public Cube(Color faceUp, Color faceDown, Color faceRight, Color faceLeft, Color faceFront, Color faceBack)
+        {
+            this.faceUp = faceUp;
+            this.faceDown = faceDown;
+            this.faceRight = faceRight;
+            this.faceLeft = faceLeft;
+            this.faceFront = faceFront;
+            this.faceBack = faceBack;
+        }
+
+        public void PitchUp()
+        {
+            Color temp = faceUp;
+
+            faceUp = faceFront;
+            faceFront = faceDown;
+            faceDown = faceBack;
+            faceBack = temp;
+        }
+        public void PitchDown()
+        {
+            Color temp = faceUp;
+
+            faceUp = faceBack;
+            faceBack = faceDown;
+            faceDown = faceFront;
+            faceFront = temp;
+        }
+
+    }
+}
